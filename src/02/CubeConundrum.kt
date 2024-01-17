f7299c1015f177ef8801d9cc6ed2eb867bd2a84b
Patrik-07 <patrik.santa07@gmail.com>
2024-01-17 14:52:28 +0100

    Improve number 2

--- src/02/CubeConundrum.kt
+++ src/02/CubeConundrum.kt
@@ -18,28 +18,33 @@ fun main() {
 }
 
 fun isGamePossible(line: String, red: Int, green: Int, blue: Int): Boolean {
-    val game = line.substringAfter(":")
-    val sets = game.split(";")
+  val game = line.substringAfter(":")
+  val sets = game.split(";")
 
-    val cubes = mutableListOf<Pair<Int, String>>()
+  val cubes = mutableListOf<Pair<String, Int>>()
 
-    for (set in sets) {
-        val cubeCounts = set.split(",").map { it.trim() }
-        for (cubeCount in cubeCounts) {
-            val cube = cubeCount.split(" ")
-            cubes.add(Pair(cube[0].toInt(), cube[1]))
-        }
-    }
+  for (set in sets) {
+      val cubeCounts = set.split(",").map { it.trim() }
+      for (cubeCount in cubeCounts) {
+          val cube = cubeCount.split(" ")
+          cubes.add(Pair(cube[1], cube[0].toInt()))
+      }
+  }
 
-    val cubeCounts = mutableMapOf("red" to red, "green" to green, "blue" to blue)
-    for (cube in cubes) {
-        val actualCubeCount = cube.first
-        val expectedCubeCount = cubeCounts[cube.second]
+  val cubeCounts = mutableMapOf("red" to red, "green" to green, "blue" to blue)
 
-        if (actualCubeCount > expectedCubeCount!!) {
-            return false
-        }
-    }
+  return isCubeCountsPossible(cubes, cubeCounts)
+}
 
-    return true
+fun isCubeCountsPossible(cubes: MutableList<Pair<String, Int>>, cubeCounts: MutableMap<String, Int>): Boolean {
+  for (cube in cubes) {
+      val expectedCubeCount = cubeCounts[cube.first]
+      val actualCubeCount = cube.second
+
+      if (actualCubeCount > expectedCubeCount!!) {
+          return false
+      }
+  }
+
+  return true
 }
